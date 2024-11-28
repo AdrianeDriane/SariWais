4c3d0e234ffcfe877547ce64a21a31207f22bf0b
AdrianeDriane <dilaoaparre@gmail.com>
2024-11-28 17:56:06 +0800

    First Commit

---
+++ src/main/java/com/g4bsit2a/sariwais/SariWais.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.g4bsit2a.sariwais;
+
+/**
+ *
+ * @author Adriane Dilao
+ */
+public class SariWais {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
